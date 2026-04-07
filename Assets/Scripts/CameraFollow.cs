90264187d21ec68d4ae80816d499365cf442c5c5
LiamColburn <liam.colburn.ny@gmail.com>
2026-04-07 00:20:48 -0700

    Comments added!

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,22 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
     public float smoothSpeed = 0.1f;
 
+    [Header("Boundaries")]
+    public float minX = 0f;
+    public float maxX = 16f;
+    public float minY = 0f;
+    public float maxY = 15f;
+
+    // Follows the set object, manually set to the player.
     void LateUpdate()
     {
         if (target == null) return;
 
-        Vector3 desired = new Vector3(target.position.x, target.position.y, -10f);
+        // Follow target
+        float x = Mathf.Clamp(target.position.x, minX, maxX);
+        float y = Mathf.Clamp(target.position.y, minY, maxY);
+
+        Vector3 desired = new Vector3(x, y, -10f);
         transform.position = Vector3.Lerp(transform.position, desired, smoothSpeed);
     }
 }
